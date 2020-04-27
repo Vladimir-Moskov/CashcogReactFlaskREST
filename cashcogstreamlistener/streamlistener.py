dd7f645bb9e7057b7f75ceb263805e182e17bbeb
Vladimir-Moskov <boba.mockob@gmail.com>
2020-04-27 16:40:48 -0400

    Launch data saver as a separate python process in order to manage better heavy stream traffic

--- cashcogstreamlistener/streamlistener.py
+++ cashcogstreamlistener/streamlistener.py
@@ -9,7 +9,7 @@ import requests
 from app.config import Config, StreamDataInjectionType
 from app.models import Expense
 from json import loads, JSONDecodeError
-from multiprocessing import Process
+from multiprocessing import Process, Queue
 
 # TODO: implement intervention reading external intervention
 
@@ -19,50 +19,64 @@ def start_read_stream():
      Run stream reader in non stop
     :return: None
     """
+    # Launch data saver as a separate python process in order to manage better heavy stream traffic
+    entyties_queue: Queue = Queue()  # entities from stream
+    # parallel process
+    saver_proc: Process = Process(target=save_expense_from_stream, args=(entyties_queue,))
+    saver_proc.daemon = True
+    saver_proc.start()
+
     r = requests.get(Config.CASHCOG_STREAM_URL, stream=True)
     for expense_line in r.iter_lines():
         if expense_line:
             try:
                 # save every item in process
                 expense_obj = loads(expense_line)
-                local_process = Process(target=save_expense_from_stream, args=(expense_obj,))
-                local_process.start()
+                entyties_queue.put(expense_obj)
+                print(f"new entity from stream {expense_obj['uuid']}")
             except (JSONDecodeError, TypeError) as error:
                 print(f"{repr(error)}")
-
+            # TODO: handle exceptions properly - replase placeholder with real code
             else:
                 pass
             finally:
                 pass
 
 
-def save_expense_from_stream(expense_obj):
+def save_expense_from_stream(entyties_queue: Queue):
     """
     save one expense item
-    :param line_obj:
-    :return:
+
+        :param entyties_queue:
+        :return:
     """
     # TODO: handle exceptions properly
-    try:
-        # direct save into DB
-        if Config.UPDATE_FROM_STREAM == StreamDataInjectionType.DIRECT_DB_DATA_INSERT:
-            Expense.query_add_from_json(expense_obj)
-        # USE 'microservices' approach
-        elif Config.UPDATE_FROM_STREAM == StreamDataInjectionType.USE_API_POST_DATA:
-            post_expence_url = Config.EXPENSE_API_ADD
-            request_self = requests.post(url=post_expence_url, json=expense_obj)
-            try:
-                if request_self.status_code == 201:
-                    print('Success!')
-                    new_item = request_self.json()
-                else:
-                    new_item = {'ERROR': request_self.status_code}
-            except Exception as error:
-                print(f"ERROR: on save_expense_from_stream PUT = {repr(error)}")
-     # logging as print for now
-        print(new_item)
-    except Exception as error:
-        print(f"{repr(error)}")
+    while True:
+        try:
+            expense_obj = entyties_queue.get()
+            if expense_obj:
+                # direct save into DB
+                if Config.UPDATE_FROM_STREAM == StreamDataInjectionType.DIRECT_DB_DATA_INSERT:
+                    Expense.query_add_from_json(expense_obj)
+                    print(f"new entity saved into DB  {expense_obj['uuid']}")
+                # USE 'microservices' approach
+                elif Config.UPDATE_FROM_STREAM == StreamDataInjectionType.USE_API_POST_DATA:
+                    post_expence_url = Config.EXPENSE_API_ADD
+                    request_self = requests.post(url=post_expence_url, json=expense_obj)
+                    try:
+                        if request_self.status_code == 201:
+                            print('Success!')
+                            new_item = request_self.json()
+                        else:
+                            new_item = {'ERROR': request_self.status_code}
+
+                        # logging as print for now
+                        print(new_item)
+                    except Exception as error:
+                        print(f"ERROR: on save_expense_from_stream PUT = {repr(error)}")
+
+        except Exception as error:
+            print(f"{repr(error)}")
 
 
 if __name__ == '__main__':
